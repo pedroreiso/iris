d4acbe75364424361485c0a7e4441f27af826c0a
pedroreiso <82176726+pedroreiso@users.noreply.github.com>
2021-04-10 17:47:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const canvas = require('discord-canvas')
 
 // Cria um cliente de inicialização da BOT
 const start = (kill = new Client()) => {
-    console.log(color('\n[DEV]', 'red'), color('- Lucas R. - KillovSky <-> +55 18 99804-4132 <-> https://chat.whatsapp.com/H53MdwhtnRf7TGX1VJ2Jje'))
-	console.log(color('[ÍRIS]', 'red'), color('Minha inicialização foi concluída, você pode usar agora...\n'))
+    console.log(color('\n[DEV]', 'red'), color('- Pedro Henrique Reis - @pedro.reiso <-> +55 31 98560-8204))
+	console.log(color('[TAIGA]', 'red'), color('Minha inicialização foi concluída, você pode usar agora...\n'))
 	
 		// Forçar recarregamento caso obtenha erros
 		kill.onStateChanged((state) => {
-			console.log('[Estado da Íris]', state)
+			console.log('[Estado da Taiga]', state)
 			if (state === 'UNPAIRED' || state === 'CONFLICT' || state === 'UNLAUNCHED') kill.forceRefocus()
 		})
 		
